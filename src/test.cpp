89273f8e259df50189dea33c175126b8cfeb3ef4
sevaphasol <sevsol2512@gmail.com>
2024-10-06 19:08:54 +0300

    Broke Stack, Added examples fow to hack it

--- src/test.cpp
+++ src/test.cpp
@@ -11,75 +11,97 @@ void* PthrPush(void* args);
 
 void* PthrDel(void* args);
 
+void  EasyHackExample();   // Ability to switch stack ids
+
+void  MediumHackExample(); // CountDataHash doesn't count last elem of data
+
+void  HardHackExample();   // When INVALID_HASH error is triggered deadlock occurs
+
+struct Stack_t
+{
+    ON_CANARY_PROTECTION(Canary_t        left_canary);
+
+    ON_DEBUG(            const char *    BornFile);
+    ON_DEBUG(            int             BornLine);
+    ON_DEBUG(            const char *    BornFunc);
+    ON_DEBUG(            const char *    name);
+    ON_HASH_PROTECTION(  uint64_t        DataHash);
+    ON_HASH_PROTECTION(  uint64_t        StructHash);
+    ON_THREAD_PROTECTION(pthread_mutex_t mutex);
+
+                         bool            inited;
+                         StackId_t       id;
+                         StackElem_t*    data;
+    ON_CANARY_PROTECTION(Canary_t*       DataLeftCanary);
+    ON_CANARY_PROTECTION(Canary_t*       DataRightCanary);
+                         uint64_t        MemorySize;
+                         uint64_t        size;
+                         uint64_t        capacity;
+
+    ON_CANARY_PROTECTION(Canary_t        right_canary);
+};
+
+extern Stack_t* STACKS[MAX_STACK_AMOUNT];
+
 StackReturnCode StackTest()
 {
-    StackId_t StackId = STACK_CTOR(MIN_STACK_SIZE);
+    // EasyHackExample();
 
-    pthread_t threads[2];
+    // MediumHackExample();
 
-//     pthread_create(&threads[0], NULL, PthrPush,  &StackId);
-//     pthread_create(&threads[1], NULL, PthrDel,   &StackId);
-//
-//     pthread_join(threads[0], NULL);
-//     pthread_join(threads[1], NULL);
-
-
-    FILE* UnitTestFile = fopen("unit_test", "wb");
-
-    if (!UnitTestFile)
-    {
-        err += INVALID_FILE_POINTER;
-
-        return FAILED;
-    }
-
-    srand((unsigned int) time(NULL));
-
-    int r = rand() % 100;
-
-    for (size_t i = 0; i < 32; i++)
-    {
-        fprintf(UnitTestFile, "%d\n", r);
-
-        StackPush(StackId, (int) r);
-
-        r = rand() % 100;
-    }
-
-    for (size_t i = 32; i > 0; i--)
-    {
-        StackPop(StackId);
-    }
-
-    StackDtor(StackId) verified;
+    // HardHackExample();
 
     return EXECUTED;
 }
 
-void* PthrPush(void* args)
+void EasyHackExample()
 {
-    StackId_t id = *((StackId_t*) args);
+    StackId_t StackId1 = STACK_CTOR(MIN_STACK_SIZE);
+    StackId_t StackId2 = STACK_CTOR(MIN_STACK_SIZE);
 
-    StackPush(id, 1);
-    StackPush(id, 2);
-    StackPush(id, 3);
-    StackPush(id, 4);
-    StackPush(id, 5);
+    StackPush(StackId1, 1) verified;
+    StackPush(StackId1, 2) verified;
+    StackPush(StackId1, 3) verified;
 
-    pthread_exit(NULL);
+    StackPush(StackId2, 4) verified;
+    StackPush(StackId2, 3) verified;
+    StackPush(StackId2, 2) verified;
+
+    Stack_t* tmp    =      STACKS[StackId1 - 1];
+    STACKS[StackId1 - 1] = STACKS[StackId2 - 1];
+    STACKS[StackId2 - 1] = tmp;
+
+    StackPush(StackId1, 4) verified;
+    StackPush(StackId2, 1) verified;
+
+    StackDtor(StackId1)    verified;
+    StackDtor(StackId2)    verified;
 }
 
-void* PthrDel(void* args)
+void MediumHackExample()
 {
-    StackId_t id = *((StackId_t*) args);
+    StackId_t StackId = STACK_CTOR(MIN_STACK_SIZE);
 
-    // *((StackElem_t*)((char*) STACKS[id - 1]->data + 3*sizeof(StackElem_t))) = 0;;
+    StackPush(StackId, 1) verified;
 
-    StackPush(id, 6);
-    StackPush(id, 7);
-    StackPush(id, 8);
-    StackPush(id, 9);
-    StackPush(id, 10);
+    Stack_t* stack = STACKS[StackId - 1];
 
-    pthread_exit(NULL);
+    stack->data[stack->capacity - 1]--;
+
+    StackPush(StackId, 1) verified;
+
+    StackDtor(StackId)    verified;
+}
+
+void HardHackExample()
+{
+    StackId_t StackId = STACK_CTOR(MIN_STACK_SIZE);
+
+    StackPush(StackId, 1) verified;
+
+    Stack_t* stack = STACKS[StackId - 1];
+
+    stack->StructHash--;
+
+    StackPush(StackId, 1) verified;
 }
